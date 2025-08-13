46047183f723aecbbda10dc03c9a5cc3effeee48
TAiGA <52752725+metarutaiga@users.noreply.github.com>
2025-08-13 23:05:06 +0800

    [x86] Move bit scan to bitwise

--- x86/x86_bitwise.cpp
+++ x86/x86_bitwise.cpp
@@ -4,6 +4,32 @@
 #include "x86_instruction.inl"
 
 //------------------------------------------------------------------------------
+void x86_instruction::BSF(Format& format, const uint8_t* opcode)
+{
+	Decode(format, opcode, "BSF", 2, 0, OPERAND_SIZE | DIRECTION);
+
+	BEGIN_OPERATION() {
+		ZF = 1;
+		if (SRC) {
+			ZF = 0;
+			DEST = std::countr_zero(SRC);
+		}
+	} END_OPERATION;
+}
+//------------------------------------------------------------------------------
+void x86_instruction::BSR(Format& format, const uint8_t* opcode)
+{
+	Decode(format, opcode, "BSR", 2, 0, OPERAND_SIZE | DIRECTION);
+
+	BEGIN_OPERATION() {
+		ZF = 1;
+		if (SRC) {
+			ZF = 0;
+			DEST = std::countl_zero(SRC);
+		}
+	} END_OPERATION;
+}
+//------------------------------------------------------------------------------
 void x86_instruction::BT(Format& format, const uint8_t* opcode)
 {
     switch (opcode[1]) {
--- x86/x86_instruction.cpp
+++ x86/x86_instruction.cpp
@@ -301,32 +301,6 @@ void x86_instruction::GS(Format& format, const uint8_t* opcode)
 //------------------------------------------------------------------------------
 //
 //------------------------------------------------------------------------------
-void x86_instruction::BSF(Format& format, const uint8_t* opcode)
-{
-    Decode(format, opcode, "BSF", 2, 0, OPERAND_SIZE | DIRECTION);
-
-    BEGIN_OPERATION() {
-        ZF = 1;
-        if (SRC) {
-            ZF = 0;
-            DEST = std::countr_zero(SRC);
-        }
-    } END_OPERATION;
-}
-//------------------------------------------------------------------------------
-void x86_instruction::BSR(Format& format, const uint8_t* opcode)
-{
-    Decode(format, opcode, "BSR", 2, 0, OPERAND_SIZE | DIRECTION);
-
-    BEGIN_OPERATION() {
-        ZF = 1;
-        if (SRC) {
-            ZF = 0;
-            DEST = std::countl_zero(SRC);
-        }
-    } END_OPERATION;
-}
-//------------------------------------------------------------------------------
 void x86_instruction::CALL(Format& format, const uint8_t* opcode)
 {
     switch (opcode[0]) {
